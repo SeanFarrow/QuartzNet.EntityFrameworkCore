02ca766694584c6a830a6076ec2f3f8be4a3ea4c
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-07-25 06:45:39 +0100

    Add a class to represent a quartz Cron trigger in the database.

---
+++ src/QuartzNet.EntityFrameworkCore/Entities/CronTrigger.cs
@@ -0,0 +1,28 @@
+﻿namespace QuartzNet.EntityFrameworkCore.Entities;
+/// <summary>
+/// A class representing a trigger that uses a cron expression.
+/// </summary>
+public class CronTrigger
+{
+    /// <summary>
+    /// The name of the scheduler for which this trigger was created.
+    /// </summary>
+    public string SchedulerName { get; }
+    
+    /// <summary>
+    /// The name of this trigger.
+    /// </summary>
+    public string TriggerName { get; }
+    /// <summary>
+    /// The group to which this <see cref="CronTrigger"/> belongs.
+    /// </summary>
+    public string TriggerGroup { get; }
+    /// <summary>
+    /// The cron expression that determines when this <see cref="CronTrigger"/> will fire.
+    /// </summary>
+    public string CronExpression { get; }
+    /// <summary>
+    /// The id of the timezone in which this <see cref="CronTrigger"/> should be fired.
+    /// </summary>
+    public string TimezoneID { get; }
+}
