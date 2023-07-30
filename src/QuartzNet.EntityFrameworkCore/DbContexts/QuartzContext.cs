c00dcbd7472f8a88c68779ba31edeb633e45cdd3
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-07-30 07:08:29 +0100

    Add a DbContext to hold Quartz.net data as well as a property that providers access to the Cron Triggers.

---
+++ src/QuartzNet.EntityFrameworkCore/DbContexts/QuartzContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuartzNet.EntityFrameworkCore.Entities;
+
+namespace QuartzNet.EntityFrameworkCore.DbContexts
+{
+    /// <summary>
+    /// DbContext for the Quartz.Net data.
+    /// </summary>
+    public class QuartzContext : DbContext
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="CronTrigger"/> instances. 
+        /// </summary>
+        public DbSet<CronTrigger> CronTriggers => Set<CronTrigger>();
+    }
+}
