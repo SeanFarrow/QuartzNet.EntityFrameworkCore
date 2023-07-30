8626ee3f6c02590395ebc6b41be8e847c98bd064
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-07-30 06:49:56 +0100

    Make all the failing tests pass for the CronTrigger classes constructor.

--- src/QuartzNet.EntityFrameworkCore/Entities/CronTrigger.cs
+++ src/QuartzNet.EntityFrameworkCore/Entities/CronTrigger.cs
@@ -27,8 +27,8 @@ public class CronTrigger
     /// <summary>
     /// The id of the timezone in which this <see cref="CronTrigger"/> should be fired.
     /// </summary>
-    public string TimezoneID { get; }
-    
+    public string? TimezoneID { get; }
+
     /// <summary>
     /// Instantiates a <see cref="CronTrigger"/>.
     /// </summary>
@@ -37,9 +37,55 @@ public class CronTrigger
     /// <param name="triggerGroup">the group to which this <see cref="CronTrigger"/> belongs.</param>
     /// <param name="cronExpression">The expression that determines when this <see cref="CronTrigger"/> will be fired.</param>
     /// <param name="timezoneId"> the id of the timezone in which the <paramref name="cronExpression"/> will be interpreted.</param>
-    /// <exception cref="NotImplementedException">The code is not yet implemented.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when the <paramref ="schedulerName"/>, <paramref ="triggerName"/>, <paramref ="triggerGroup"/> or <paramref ="cronExpression"/> are <c>null</c>.</exception>
+    /// <exception cref="ArgumentException">Thrown when the <paramref ="schedulerName"/>, <paramref ="triggerName"/>, <paramref ="triggerGroup"/> or <paramref ="cronExpression"/> are empty strings.</exception>
     public CronTrigger(string schedulerName, string triggerName, string triggerGroup, string cronExpression, string timezoneId)
     {
-        throw new NotImplementedException();
+        if (schedulerName is null)
+        {
+            throw new ArgumentNullException(nameof(schedulerName));
+        }
+
+        if (schedulerName.Length is 0)
+        {
+            throw new ArgumentException("The schedulerName parameter cannot be an empty string.", nameof(schedulerName));
+        }
+        SchedulerName =schedulerName;
+        
+        if (triggerName is null)
+        {
+            throw new ArgumentNullException(nameof(triggerName));
+        }
+
+        if (triggerName.Length is 0)
+        {
+            throw new ArgumentException("The triggerName parameter cannot be an empty string.", nameof(triggerName));
+        }
+        TriggerName = triggerName;
+        
+        if (triggerGroup is null)
+        {
+            throw new ArgumentNullException(nameof(triggerGroup));
+        }
+
+        if (triggerGroup.Length is 0)
+        {
+            throw new ArgumentException("The triggerGroup parameter cannot be an empty string.", nameof(triggerGroup));
+        }
+        TriggerGroup = triggerGroup;
+
+        if (cronExpression is null)
+        {
+            throw new ArgumentNullException(nameof(cronExpression));
+        }
+
+        if (cronExpression.Length is 0)
+        {
+            throw new ArgumentException("The cronExpression parameter cannot be an empty string.", nameof(cronExpression));
+        }
+
+        CronExpression = cronExpression;
+
+        TimezoneID = timezoneId;
     }
 }
