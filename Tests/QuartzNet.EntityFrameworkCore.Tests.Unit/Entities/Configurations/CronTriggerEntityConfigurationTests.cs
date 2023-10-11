b840fa4586fd0cf314c3bdec2ca5530a3503ed5c
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-10-11 05:50:12 +0100

    Configure the CronTrigger entoty type.

---
+++ Tests/QuartzNet.EntityFrameworkCore.Tests.Unit/Entities/Configurations/CronTriggerEntityConfigurationTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.EntityFrameworkCore;
+using QuartzNet.EntityFrameworkCore.Entities;
+using QuartzNet.EntityFrameworkCore.Entities.Configurations;
+
+namespace QuartzNet.EntityFrameworkCore.Tests.Unit.Entities.Configurations;
+
+public class CronTriggerEntityConfigurationTests : EntityTypeConfigurationTestsBase<CronTriggerEntityConfiguration, CronTrigger>
+{
+    [Fact]
+    public void Configure_ConfiguresTheCronTriggerEntotyeAsExpected()
+    {
+        var sut = CreateSut();
+        var entityBuilder = GetEntityBuilder();
+
+        sut.Configure(entityBuilder);
+        var metadata = entityBuilder.Metadata;
+
+        using var scope = new AssertionScope();
+        metadata.GetSchema().Should().Be("Quartz");
+        metadata.GetTableName().Should().Be("CronTriggers");
+        var schedulerNameProperty = metadata.FindDeclaredProperty("SchedulerName");
+        ValidateModelProperty(schedulerNameProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().Be(120);
+            x?.GetContainingKeys().Should().HaveCount(1);
+        });
+
+        var triggerNameProperty = metadata.FindDeclaredProperty("TriggerName");
+        ValidateModelProperty(triggerNameProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().Be(150);
+            x?.GetContainingKeys().Should().HaveCount(1);
+        });
+
+        var triggerGroupProperty = metadata.FindDeclaredProperty("TriggerGroup");
+        ValidateModelProperty(triggerGroupProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().Be(150);
+            x?.GetContainingKeys().Should().HaveCount(1);
+        });
+
+        var CronExpressionProperty = metadata.FindDeclaredProperty("CronExpression");
+        ValidateModelProperty(CronExpressionProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().Be(120);
+        });
+
+        var timezoneIDProperty = metadata.FindDeclaredProperty("TimezoneId");
+        ValidateModelProperty(timezoneIDProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().Be(80);
+        });
+    }
+}
---
+++ src/QuartzNet.EntityFrameworkCore/Entities/Configurations/CronTriggerEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuartzNet.EntityFrameworkCore.Entities.Configurations;
+
+public class CronTriggerEntityConfiguration : IEntityTypeConfiguration<CronTrigger>
+{
+    public void Configure(EntityTypeBuilder<CronTrigger> builder)
+    {
+        builder.ToTable("CronTriggers", "Quartz");
+        builder.HasKey("SchedulerName", "TriggerName", "TriggerGroup");
+        builder.Property(x =>x.SchedulerName).IsRequired().HasMaxLength(120);
+        builder.Property(x => x.TriggerName).IsRequired().HasMaxLength(150);
+        builder.Property(x => x.TriggerGroup).IsRequired().HasMaxLength(150);
+        builder.Property(x => x.CronExpression).IsRequired().HasMaxLength(120);
+        builder.Property(x => x.TimezoneId).IsRequired().HasMaxLength(80);
+    }
+}
