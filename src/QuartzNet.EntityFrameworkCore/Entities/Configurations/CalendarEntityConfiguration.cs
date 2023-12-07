28b3e532eb41a4cbc6e13860266aeb00803edd7c
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-12-07 09:00:31 +0000

    Add a class that configures a Calendar entity.

---
+++ src/QuartzNet.EntityFrameworkCore/Entities/Configurations/CalendarEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace QuartzNet.EntityFrameworkCore.Entities.Configurations;
+
+public class CalendarEntityConfiguration : IEntityTypeConfiguration<Calendar>
+{
+    public void Configure(EntityTypeBuilder<Calendar> builder)
+    {
+        builder.ToTable("Calendars", "Quartz");
+        builder.HasKey("SchedulerName", "CalendarName");
+        builder.Property(x => x.SchedulerName).IsRequired().HasMaxLength(120);
+        builder.Property(x => x.CalendarName).IsRequired().HasMaxLength(200);
+        builder.Property(x => x.SerializedCalendar).IsRequired();
+    }
+}
---
+++ tests/QuartzNet.EntityFrameworkCore.Tests.Unit/Entities/Configurations/CalendarEntityConfigurationTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.EntityFrameworkCore;
+using QuartzNet.EntityFrameworkCore.Entities;
+using QuartzNet.EntityFrameworkCore.Entities.Configurations;
+
+namespace QuartzNet.EntityFrameworkCore.Tests.Unit.Entities.Configurations;
+
+public class CalendarEntityConfigurationTests : EntityTypeConfigurationTestsBase<CalendarEntityConfiguration, Calendar>
+{
+    [Fact]
+    public void Configure_ConfiguresTheCalendarEntityAsExpected()
+    {
+        var sut = CreateSut();
+        var entityBuilder = GetEntityBuilder();
+
+        sut.Configure(entityBuilder);
+        var metadata = entityBuilder.Metadata;
+
+        using var scope = new AssertionScope();
+        metadata.GetSchema().Should().Be("Quartz");
+        metadata.GetTableName().Should().Be("Calendars");
+        var schedulerNameProperty = metadata.FindDeclaredProperty("SchedulerName");
+        ValidateModelProperty(schedulerNameProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().Be(120);
+            x?.GetContainingKeys().Should().HaveCount(1);
+        });
+
+        var calendarNameProperty = metadata.FindDeclaredProperty("CalendarName");
+        ValidateModelProperty(calendarNameProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().Be(200);
+            x?.GetContainingKeys().Should().HaveCount(1);
+        });
+
+        var serializedCalendarProperty = metadata.FindDeclaredProperty("SerializedCalendar");
+        ValidateModelProperty(serializedCalendarProperty, x =>
+        {
+            x?.IsNullable.Should().BeFalse();
+            x?.GetMaxLength().Should().BeNull();
+            x?.GetContainingKeys().Should().BeEmpty();
+        });
+    }
+}
