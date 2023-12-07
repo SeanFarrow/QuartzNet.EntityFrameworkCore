8be1d84dacaf85710460f8bfb1c94df9b18bc45c
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2023-12-07 08:39:54 +0000

    Add an entity class to represent a Quartz calendar.

---
+++ src/QuartzNet.EntityFrameworkCore/Entities/Calendar.cs
@@ -0,0 +1,64 @@
+﻿namespace QuartzNet.EntityFrameworkCore.Entities;
+/// <summary>
+/// A class representing a calendar.
+/// </summary>
+public class Calendar
+{
+    /// <summary>
+    /// The name of the scheduler for which this trigger was created.
+    /// </summary>
+    public string SchedulerName { get; }
+
+    /// <summary>
+    /// The name of this calendar.
+    /// </summary>
+    public string CalendarName { get; }
+
+    /// <summary>
+    /// The serialized representation of this <see cref="SerializedCalendar"/>.
+    /// </summary>
+    public string SerializedCalendar { get; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="schedulerName">The scheduler to which this <see cref="SerializedCalendar"/> belongs.</param>
+    /// <param name="calendarName">The name of this <see cref="SerializedCalendar"/></param>
+    /// <param name="serializedCalendar">The serialized representation of this <see cref="SerializedCalendar"/>.</param>
+    public Calendar(string schedulerName, string calendarName, string serializedCalendar)
+    {
+        if (schedulerName is null)
+        {
+            throw new ArgumentNullException(nameof(schedulerName));
+        }
+        
+        if (schedulerName.Length is 0)
+        {
+            throw new ArgumentException("The schedulerName parameter cannot be an empty string.", nameof(schedulerName));
+        }
+        
+        if (calendarName is null)
+        {
+            throw new ArgumentNullException(nameof(calendarName));
+        }
+
+        if (calendarName.Length is 0)
+        {
+            throw new ArgumentException("The calendarName parameter cannot be an empty string.", nameof(calendarName));
+        }
+        
+        if (serializedCalendar is null)
+        {
+            throw new ArgumentNullException(nameof(serializedCalendar));
+        }
+
+        if (serializedCalendar.Length is 0)
+        {
+            throw new ArgumentException("The serializedCalendar parameter cannot be an empty string.", nameof(serializedCalendar));
+        }
+        
+        SchedulerName =schedulerName;
+        CalendarName =calendarName;
+        SerializedCalendar =serializedCalendar;
+    }
+}
---
+++ tests/QuartzNet.EntityFrameworkCore.Tests.Unit/Entities/CalendarTests.cs
@@ -0,0 +1,84 @@
+﻿using FluentAssertions;
+using QuartzNet.EntityFrameworkCore.Entities;
+
+namespace QuartzNet.EntityFrameworkCore.Tests.Unit.Entities;
+
+public class CalendarTests
+{
+    #region Private members.
+    private string _schedulerName ="test scheduler";
+    private string _calendarName ="test calendar";
+    private string _serializedCalendar ="serialized calendar";
+
+    private Calendar CreateSut() => new(_schedulerName, _calendarName, _serializedCalendar);
+    #endregion
+
+    #region Constructor tests.
+    [Fact]
+    public void AnArgumentNullExceptionShouldBeThrownWhenTheSchedulerNamePassedInIsNull()
+    {
+        _schedulerName = null!;
+
+        var act = CreateSut;
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AnArgumentExceptionShouldBeThrownWhenTheSchedulerNamePassedInIsAnEmptyString()
+    {
+        _schedulerName = String.Empty;
+
+        var act = CreateSut;
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void AnArgumentNullExceptionShouldBeThrownWhenTheCalendarNamePassedInIsNull()
+    {
+        _calendarName = null!;
+
+        var act = CreateSut;
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AnArgumentExceptionShouldBeThrownWhenTheCalendarNamePassedInIsAnEmptyString()
+    {
+        _calendarName = String.Empty;
+
+        var act = CreateSut;
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void AnArgumentNullExceptionShouldBeThrownWhenTheSerializedCalendarPassedInIsNull()
+    {
+        _serializedCalendar = null!;
+
+        var act = CreateSut;
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AnArgumentExceptionShouldBeThrownWhenTheSerializerCalendarPassedInIsAnEmptyString()
+    {
+        _serializedCalendar = String.Empty;
+
+        var act = CreateSut;
+
+        act.Should().Throw<ArgumentException>();
+    }
+    [Fact]
+    public void ACalendarInstanceShouldBeInstantiatedWhenAllParametersAreValid()
+    {
+        var result = CreateSut();
+
+        result.Should().NotBeNull();
+    }
+    #endregion
+}
